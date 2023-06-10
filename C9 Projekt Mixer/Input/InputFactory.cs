a5d695e7a1efdbfbeb1aea20a4bae57624c769b3
bartekwasik <bartekwasik@student.agh.edu.pl>
2023-06-10 14:09:07 +0200

    -InputFacotory

---
+++ C9 Projekt Mixer/Input/InputFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9_Projekt_Mixer.Panorama
+{
+    internal class InputFactory
+    {
+        public Input CreateInput()
+        {
+            return new Input(0, false, false, false);
+        }
+    }
+}
